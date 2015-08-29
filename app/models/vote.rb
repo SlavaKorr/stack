9f1079ed9cb0c8b949b9f50bed11a928ec56bf70
Vladislav Korotkov <usa89@mail.ru>
2015-08-30 01:56:18 +0300

    cache

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
 
-  belongs_to :votable, polymorphic: true
+  belongs_to :votable, polymorphic: true, touch: true
   belongs_to :user
   validates :value, :user_id, presence: true
 
